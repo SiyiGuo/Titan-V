ae0a5a7385620da62cd6114d6a89460bdc8c2461
Raymond <1578942650@qq.com>
2018-03-14 20:56:33 +1100

    Adding check_Valid_jump

--- partA/partA.py
+++ partA/partA.py
@@ -12,41 +12,74 @@ class BoardReader(object):
             line = input().split(" ")
             self.board.append(self.processLine(line))
         
-        print(self.board)
-
         mode = input()
         self.mode = mode
 
-        return self.mode, self.board
+        return self.board, self.mode
+
+"""Notation Area"""
+EMPTY = "-"
+BANNED = "X"
+WHITE = "O"
+BLACK = "@"
 
 class Board(object):
     
     __directions = {
-        "top": (-1,0)
-        "bot": (1,0)
-        "left": (0,-1)
+        "top": (-1,0),
+        "bot": (1,0),
+        "left": (0,-1),
         "right": (0,1)
     }
 
-    """Notation Area"""
-    EMPTY = "-"
-    BANNED = "X"
-    WHITE = "O"
-    BLACK = "@"
-    def __intit__(self,n, canonicalBoard = None):
+    __jumpDirections = {
+        "top": (-2,0),
+        "bot": (2,0),
+        "left": (0,-2),
+        "right": (0,2)
+    }
+
+    def __init__(self, n, canonicalBoard = None):
         """board setup"""
         """coordinate system: (row, column)"""
 
         self.n = n
-        if canoicalBoard is not None:
+        if canoicalBoard is None:
             self.pieces = [[EMPTY]*self.n]*self.n
             self.pieces[0][0] = BANNED #top left 
-            self.pieces[0][self.n] = BANNED #top right
-            self.pieces[self.n - 1][0] = BANNED #bottom left
-            self.pieces[self.n - 1][self.n - 1] = BANNED #bottom right corner
+            self.pieces[0][self.n - 1] = BANNED #top right
+            self.pieces[self.n - 2][0] = BANNED #bottom left
+            self.pieces[self.n - 2][self.n - 2] = BANNED #bottom right corner
         else:
             self.pieces = canonicalBoard
     
+    def _check_valid_jump(self, piecePosition, direction):
+        """
+        Given a jump direction
+        return Valid, move
+        """
+        x_orig, y_orig = piecePosition
+
+        x_dir, y_dir = direction #find the direction
+        x_next, y_next = (x_orig + x_dir // 2, y_orig + y_dir // 2) #find the piece next to this piece
+        
+        x_dest, y_dest = (x_orig + x_dir,y_orig + y_dir)
+        try:
+            #check whether there is a piece next to it
+            if self.pieces[x_next][y_next] is EMPTY or self.pieces[x_next][y_next] is BANNED:
+                #the case it should not mode
+                print("fail"+str((x_dest, y_dest)+str((x_next, y_))))
+                return False, None
+            else:
+                #the case white of black piece is next to it
+                if self.pieces[x_dest][y_dest] is EMPTY:
+                    return True, (x_dest, y_dest)
+                else:
+                    return False, None
+        except:
+            #cover the case index out of range
+            return False, None
+
     def _check_valid_move(self, piecePosition, direction):
         """
         check whether this direction if valid to move
@@ -57,30 +90,16 @@ class Board(object):
         x_dir, y_dir = direction
         x_dest, y_dest = (x_orig + x_dir,y_orig + y_dir)
 
-        if x_dest < 0 or x_dest > self.n:
+        try:
+            if self.pieces[x_dest][y_dest] is EMPTY:
+                # empty place, can move
+                return True, (x_dest, y_dest)
+            else:
+                #cover other case
+                return False, None
+        except:
+            #cover the case index out of range
             return False, None
-        else if y_dest < 0 or y_dest > self.n:
-            return False, None
-        else if self.pieces[x_dest][y_dest] is BANNED:
-            return False, None
-        else if self.pieces[x_dest][y_dest] is EMPTY:
-            # empty place, can move
-            return True, (x_dest, y_dest)
-        else if self.pieces[x_dest][y_dest] == self.pieces[x_orig][y_orig]:
-            #if destination is the same piece
-
-            #TODO: see if we can make a jump
-            return False, None
-
-        else if self.pieces[x_dest][y_dest] != self.pieces[x_orig][y_orig]:
-            #if destination is not the same piece
-
-            #TODO: see if we can make a jump
-            return False, None
-
-
-        #now it is the valid case
-        return True, (x_dest, y_dest)
 
 
 
@@ -98,7 +117,12 @@ class Board(object):
         # to be returned
         moves = []
         for direction in self.__directions.values():
-            valid, move = self._check_valid_move(piecePosition, direction):
+            valid, move = self._check_valid_move(piecePosition, direction)
+            if valid:
+                moves.append(move)
+        
+        for jumpDirection in self.__jumpDirections.values():
+            valid,move = self._check_valid_jump(piecePosition, jumpDirection)
             if valid:
                 moves.append(move)
         
@@ -108,7 +132,15 @@ class Board(object):
         """
         Given a color, return all the legal moves
         """
+        moves = set()
 
+        for x in range(self.n):
+            for y in range(self.n):
+                if self.pieces[x][y] == color:
+                    newMoves = self.getValidMoveForPiece((x,y))
+                    moves.update(newMoves)
+            
+        return list(moves)
 
 
     
@@ -118,8 +150,10 @@ class Board(object):
 boardReader = BoardReader()
 canoicalBoard, mode = boardReader.readInput()
 
-board = Board(canoicalBoard)
-
+board = Board(8, canoicalBoard)
+print(board.pieces)
+print(len(board.getAllLegalMoves(WHITE)))
+print(len(board.getAllLegalMoves(BLACK)))
 
 
 
