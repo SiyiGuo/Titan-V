b7efcf5f9b71c46c4136528683aacf6ff8f52d26
Unknown <1578942650@qq.com>
2018-03-19 21:34:43 +1100

    HalfGo Hack for the self play against each other

--- AlphaHalfGo-Zero/Coach.py
+++ AlphaHalfGo-Zero/Coach.py
@@ -131,8 +131,8 @@ class Coach():
 
             #OLD VS NEW
             print('PITTING AGAINST PREVIOUS VERSION')
-            arena = Arena(lambda x: np.argmax(pmcts.getActionProb(x, temp=0)),
-                          lambda x: np.argmax(nmcts.getActionProb(x, temp=0)), self.game)
+            arena = Arena(lambda x: np.argmax(pmcts.getActionProb(x, 0, temp=0)),
+                          lambda x: np.argmax(nmcts.getActionProb(x, 0, temp=0)), self.game)
             pwins, nwins, draws = arena.playGames(self.args.arenaCompare) #playing new mode against old models
 
             print('NEW/PREV WINS : %d / %d ; DRAWS : %d' % (nwins, pwins, draws))
