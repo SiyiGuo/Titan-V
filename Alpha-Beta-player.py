dc2a204a572ef68d4a9baecbb9e6ef4e352e93e5
Ian Fan <yubanfan@gmail.com>
2018-04-28 15:13:08 +1000

    first commit of alpha beta player

---
+++ Alpha-Beta-player.py
@@ -0,0 +1,11 @@
+from AlphaPubg-Zero/Pubg/PubgGame import PubgGame as pubg
+
+def sample_player():
+
+    def __init__(player = 1):
+        self.player = player
+
+   
+
+    def alphabeta_search(board, depth, a, b):
+        if depth == 0 or 
