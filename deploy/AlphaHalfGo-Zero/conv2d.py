eae4320dcda88102ca125c6537be42916089b50c
Ian Fan <yubanfan@gmail.com>
2018-05-06 01:34:01 +1000

    0.3 process second

--- deploy/AlphaHalfGo-Zero/conv2d.py
+++ deploy/AlphaHalfGo-Zero/conv2d.py
@@ -67,7 +67,7 @@ def conv_forward(A_prev, W, b, hparameters):
                 horiz_end = horiz_start + f
                     
                 # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
-                a_slice_prev = A_prev_pad[vert_start:vert_end,horiz_start:horiz_end]
+                a_slice_prev = A_prev_pad[vert_start:vert_end,horiz_start:horiz_end,:]
                    
                 # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron. (≈1 line)
                 Z[h, w, c] = conv_single_step(a_slice_prev, W[:,:,:,c], b[c])
@@ -76,7 +76,6 @@ def conv_forward(A_prev, W, b, hparameters):
     
     # Making sure your output shape is correct
     assert(Z.shape == (n_H,n_W,n_C))
-    
     # Save information in "cache" for the backprop
     cache = (A_prev, W, b, hparameters)
     
@@ -96,19 +95,88 @@ def batchnorm_forward(X, gamma, beta):
 def ReLU(x):
     return np.maximum(x, 0)
 
+def conv_forward1(X, W, b, stride = 1, padding = 1):
+    h_filter, w_filter, d_filter, n_filters  = W.shape
+    h_x, w_x, c_x  = X.shape
+    
+    h_out = (h_x - h_filter + 2*padding) / stride + 1
+    w_out = (w_x - w_filter + 2*padding) / stride + 1
+    h_out, w_out = int(h_out), int(w_out)
+
+    X_col = im2col_indices(X, h_filter, w_filter, padding = padding, stride = stride) # 8*8*1 -> 9*64
+    W_col = W.transpose(3,2,0,1) # 3*3*1*512 -> 512 * 9
+    
+    W_col = W_col.reshape(n_filters, -1)
+
+    b_col = b.reshape(b.shape[0],-1)  # 512, -> 512*1
+
+    out = W_col @ X_col #512*9 dot 9*64 -> 512*64
+
+    out += b_col
+
+    out = out.reshape(n_filters, h_out, w_out).transpose(1,2,0)
+    
+    return out
+
+def im2col_indices(x, field_height, field_width, padding = 1, stride = 1):
+    p = padding
+    x_padded = np.pad(x, ((p,p),(p,p),(0,0)), mode = 'constant')
+
+    k,i,j = get_im2col_indices(x.shape, field_height, field_width, padding, stride)
+   
+    cols = x_padded[i,j,k]
+    
+    C = x.shape[2]
+    # print(cols)
+    cols = cols.reshape(field_height * field_width * C, -1)
+    return cols
+
+def get_im2col_indices(x_shape, field_height, field_width, padding = 1, stride = 1):
+    H,W,C = x_shape
+    
+    assert(H+2*padding - field_height) % stride == 0
+    assert(W+2*padding - field_width) % stride == 0
+    out_height = int((H + 2*padding - field_height) / stride + 1)
+    out_width = int((H + 2*padding - field_width) / stride + 1)
+
+    i0 = np.repeat(np.arange(field_height),field_width)
+    i0 = np.tile(i0,C)
+    i1 = stride * np.repeat(np.arange(out_height), out_width)
+    j0 = np.tile(np.arange(field_width), field_height * C)
+    j1 = stride * np.tile(np.arange(out_width), out_height)
+    i = i0.reshape(-1,1) + i1.reshape(1,-1)
+    j = j0.reshape(-1,1) + j1.reshape(1,-1)
+
+    k = np.repeat(np.arange(C),field_height*field_width).reshape(-1,1)
+
+    return (k.astype(int),i.astype(int),j.astype(int))
+    
+
 def conv2d(data, pad, stride, wPath, biasPath, gPath, bPath, param):
     weight = param[wPath]
     bias = param[biasPath]
     g = param[gPath]
     b = param[bPath]
 
-    data = conv_forward(x_image, weight, bias, {"pad": pad, "stride": stride})
+    # data = conv_forward(data, weight, bias, {"pad": pad, "stride": stride})
+    data = conv_forward1(data, weight, bias, pad, stride)
+
     data = batchnorm_forward(data, g, b)
     data = ReLU(data)
     return data
 
-    
-s = time.time()
+def conv2d1(data, pad, stride, wPath, biasPath, gPath, bPath, param):
+    weight = param[wPath]
+    bias = param[biasPath]
+    g = param[gPath]
+    b = param[bPath]
+
+    data = conv_forward(data, weight, bias, {"pad": pad, "stride": stride})
+    # data = conv_forward1(data, weight, bias, pad, stride)
+    data = batchnorm_forward(data, g, b)
+    data = ReLU(data)
+    return data    
+
 param = {}
 files = ["conv2dkernel:0.npy", "conv2dbias:0.npy", "batch_normalizationgamma:0.npy", "batch_normalizationbeta:0.npy", 
 "conv2d_1kernel:0.npy", "conv2d_1bias:0.npy", "batch_normalization_1gamma:0.npy", "batch_normalization_1beta:0.npy",
@@ -118,15 +186,31 @@ files = ["conv2dkernel:0.npy", "conv2dbias:0.npy", "batch_normalizationgamma:0.n
 for file in files:
     p = np.load(file)
     param[file] = p
-e =time.time()
 
-data = [1] * 64
+s = time.time()
+data = [0]+[1] * 63
 x_image = np.array(data).reshape(8,8,1)
 data = conv2d(x_image, 1, 1, "conv2dkernel:0.npy", "conv2dbias:0.npy", "batch_normalizationgamma:0.npy", "batch_normalizationbeta:0.npy", param)
-data = conv2d(x_image, 1, 1, "conv2d_1kernel:0.npy", "conv2d_1bias:0.npy", "batch_normalization_1gamma:0.npy", "batch_normalization_1beta:0.npy", param)
-data = conv2d(x_image, 1, 1, "conv2d_2kernel:0.npy", "conv2d_2bias:0.npy", "batch_normalization_2gamma:0.npy", "batch_normalization_2beta:0.npy", param)
-data = conv2d(x_image, 1, 1, "conv2d_3kernel:0.npy", "conv2d_3bias:0.npy", "batch_normalization_3gamma:0.npy", "batch_normalization_3beta:0.npy", param)
-data = conv2d(x_image, 1, 1, "conv2d_4kernel:0.npy", "conv2d_4bias:0.npy", "batch_normalization_4gamma:0.npy", "batch_normalization_4beta:0.npy", param)
-
+data = conv2d(data, 1, 1, "conv2d_1kernel:0.npy", "conv2d_1bias:0.npy", "batch_normalization_1gamma:0.npy", "batch_normalization_1beta:0.npy", param)
+data = conv2d(data, 1, 1, "conv2d_2kernel:0.npy", "conv2d_2bias:0.npy", "batch_normalization_2gamma:0.npy", "batch_normalization_2beta:0.npy", param)
+data = conv2d(data, 1, 1, "conv2d_3kernel:0.npy", "conv2d_3bias:0.npy", "batch_normalization_3gamma:0.npy", "batch_normalization_3beta:0.npy", param)
+data = conv2d(data, 1, 1, "conv2d_4kernel:0.npy", "conv2d_4bias:0.npy", "batch_normalization_4gamma:0.npy", "batch_normalization_4beta:0.npy", param)
+e =time.time()
 print(s-e)
-print(data.shape)
+
+s = time.time()
+data1 = [0]+[1] * 63
+x_image = np.array(data1).reshape(8,8,1)
+data1 = conv2d1(x_image, 1, 1, "conv2dkernel:0.npy", "conv2dbias:0.npy", "batch_normalizationgamma:0.npy", "batch_normalizationbeta:0.npy", param)
+data1 = conv2d1(data1, 1, 1, "conv2d_1kernel:0.npy", "conv2d_1bias:0.npy", "batch_normalization_1gamma:0.npy", "batch_normalization_1beta:0.npy", param)
+data1 = conv2d1(data1, 1, 1, "conv2d_2kernel:0.npy", "conv2d_2bias:0.npy", "batch_normalization_2gamma:0.npy", "batch_normalization_2beta:0.npy", param)
+data1 = conv2d1(data1, 1, 1, "conv2d_3kernel:0.npy", "conv2d_3bias:0.npy", "batch_normalization_3gamma:0.npy", "batch_normalization_3beta:0.npy", param)
+data1 = conv2d1(data1, 1, 1, "conv2d_4kernel:0.npy", "conv2d_4bias:0.npy", "batch_normalization_4gamma:0.npy", "batch_normalization_4beta:0.npy", param)
+e =time.time()
+print(s-e)
+
+print(data[-1][-1][-1])
+print("----------------")
+print(data1[-1][-1][-1])
+print("------------")
+print(data[-1][-1][-1] == data1[-1][-1][-1])
