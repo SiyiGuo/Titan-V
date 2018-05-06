620783469476f1953aaae98aeea74dee27ca3cac
Ian Fan <yubanfan@gmail.com>
2018-05-06 20:52:47 +1000

    Added player and referee file

---
+++ deploy/AlphaHalfGo-Zero/Player.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class Player():
+
+    def __init__(self, color):
+        self.player = color
+        self.board = np.zeros([8,8])
+
+    def update(self,action):
+
+
+    def action(self, turn):
+        
